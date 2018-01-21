2b100e35c54619cdaa6de0aaeefb8cc1e4db5167
unknown <bermartinv@gmail.com>
2018-01-21 23:49:10 +0100

    Finish mockup

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # mockup2
 <<<<<<< HEAD
-![imagen mockup](images/mockup.jpg"imagen mockup")
-=======
-![mockup](/images/mockup.jpg)
->>>>>>> 945b43e5f83e5d3182c143a7bd64ca148c2e5f44
+![imagen mockup](https://bermartinv.github.io/mockup2/images/mockup.jpg"imagen mockup")
---
+++ js/scroll.js
@@ -0,0 +1,23 @@
+let arrow = document.getElementById('arrow');
+
+// HIDE OR SHOW ARROW
+window.addEventListener('scroll',function(){
+	let scrollNumber = window.pageYOffset;
+	if (scrollNumber > 400){
+		arrow.style.display = 'block';
+	}else{
+		arrow.style.display = 'none';
+	}
+});
+arrow.addEventListener('click',function(){
+	let interval = setInterval(subir,100);
+	function subir(){
+		let scrollNumber = window.pageYOffset;
+		if (scrollNumber < 10){
+			clearInterval(interval);
+		}else{
+			window.scrollBy(0,-90);
+		}
+	}
+});
+
